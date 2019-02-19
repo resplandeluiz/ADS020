8fd08762a3db024d1ac9e6ab645a4850790049b8
17114290048 <17114290048@OLAB05-D05.academico.iesb.br>
2019-02-19 20:35:33 -0300

    Estanciando a classe gerente

---
+++ Atividade01/test/TestaEmpresa.java
@@ -0,0 +1,12 @@
+
+public class TestaEmpresa {
+    
+    Empresa empresa = new Empresa(10);    
+    Funcionario gerente1 = new Funcionario(1711,"Resplande",Funcionario.SEXO_MASCULINO,1950.40);
+    
+    empresa.contratarFuncionario(Funcionario gerente1);
+    
+    
+    
+    
+}
