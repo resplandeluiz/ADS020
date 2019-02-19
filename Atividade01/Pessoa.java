c307450b4a8cac4db2735207d2e280f447feb05b
17114290048 <17114290048@OLAB05-D05.academico.iesb.br>
2019-02-18 21:52:58 -0300

    2 letter g

---
+++ Atividade01/Pessoa.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 17114290048
+ */
+public class Pessoa {
+    
+    public static int SEXO_MASCULINO  = 1;
+    public static int SEXO_FEMININO = 2;
+     
+    private String Nome;
+    private int Sexo;
+    public String getNome() { return Nome; }
+    public void setNome(String Nome) { this.Nome = Nome; }
+
+    public int getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(int Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    
+
+     public Pessoa(String Nome, int Sexo) {        
+            this.Nome = Nome;
+            this.Sexo = Sexo;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        new Pessoa('Luiz Resplande',Pessoa.SEXO_MASCULINO);
+
+    }
+    
+}
