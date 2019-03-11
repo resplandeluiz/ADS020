fe4089b26fc9c9f113d47ca0aaa9c3f776908bef
17114290048 <17114290048@OLAB05-D05.academico.iesb.br>
2019-03-11 20:08:51 -0300

    Adicionando ConexaoBD em Atividade04

---
+++ Atividade04/src/java/modelo/persistencia/ConexaoBD.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author 17114290048
+ */
+public class ConexaoBD {
+
+    private static Connection conexao;
+
+    public static Connection getConexao() throws DadosException {
+
+        if (conexao == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                conexao = DriverManager.getConnection("jdbc:mysql://localhost/cadastro", "root", "");
+
+            } catch (ClassNotFoundException ex) {
+                throw new DadosException("Erro ao carregar JDBC!");
+            } catch (SQLException ex) {
+                throw new DadosException("Erro ao conectar MySQL!");
+            }
+
+        }
+        return conexao;
+
+    }
+}
