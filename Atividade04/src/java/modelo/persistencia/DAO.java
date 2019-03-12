28063c3d24c989942494581a22d57b86c6d0dcda
17114290048 <17114290048@OLAB05-D05.academico.iesb.br>
2019-03-11 21:08:35 -0300

    Adicionando DAO e AlunoDAO em Atividade04

---
+++ Atividade04/src/java/modelo/persistencia/DAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.persistencia;
+
+import java.util.List;
+
+/**
+ *
+ * @author 17114290048
+ */
+public interface DAO<T>{
+    
+    List<T> listar() throws DadosException;
+    void incluir(T entidade) throws DadosException;
+    void alterar(T entidade) throws DadosException;
+    void excluir(int id) throws DadosException;
+    T consultar(int id) throws DadosException;
+    
+    
+}
